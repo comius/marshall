6448441af7d66377ad5882753ca07cd1ef5b806a
Ivo List <ivo.list@guest.arnes.si>
2013-05-28 10:16:27 +0200

    proof of concept

--- src/compiler.ml
+++ src/compiler.ml
@@ -8,12 +8,12 @@ struct
     | Sigma of approx * approx * refine * basesets
     | Tuple of expr list (* [(e1, ..., en)] *)
     | Uncompiled of S.expr  
-  and eval_real = prec:int -> round:D.rounding_mode -> basesets -> I.t
-  and approx = basesets -> bool
-  and refine = basesets -> basesets
-  and basesets = env list
-  and env = Quant of S.name * I.t * basesets | Cut of S.name * I.t * basesets * basesets   
-
+  and eval_real = prec:int -> round:D.rounding_mode -> env -> basesets -> I.t
+  and approx = prec:int -> env -> basesets -> bool
+  and refine = k:int -> prec:int -> env -> basesets -> basesets
+  and basesets = bs list
+  and bs = Quant of S.name * (I.t * basesets) list | Cut of S.name * I.t * basesets * basesets   
+  and env = (S.name * I.t) list
   and arrow = 
      S.name * S.ty * expr (* Concrete syntax is [fun x : ty => e] *)
 
@@ -29,6 +29,13 @@ struct
             List.nth lst k
           with Failure _ -> error "Tuple too short")
       | _ -> error "Tuple expected"
+
+  let make_prec prec i =
+    let w = I.width ~prec:2 ~round:D.up i in
+    let e1 = D.get_exp w in
+    let e2 = max (D.get_exp (I.lower i)) (D.get_exp (I.upper i)) in
+      max 2 (max prec (- 5 * (e1 - e2) / 4))
+
 	
   let rec compile env e = match e with    
     | S.Proj (e, k) -> 
@@ -55,17 +62,47 @@ struct
 	     with Not_found ->
 	       error ("Unknown variable " ^ S.string_of_name x))
   and
-    idref = fun bs -> bs  
+    idref = fun ~k ~prec env bs -> bs  
+  and
+    cutref l1 r1 l2 r2 = fun ~k ~prec env bs -> 
+	    match bs with
+	    | [Cut (x,i,bs1,bs2)] ->
+	      let prec = make_prec prec i in
+		(* To refine a cut [Cut(x,i,p1,p2)] we try to make the
+		   interval [i] smaller and refine [p1] and [p2]. *)
+	      let a = I.lower i in
+	      let b = I.upper i in
+		(* Bisection *)
+	      let m1, m2 = I.thirds prec k i in
+	      let a' = (if l1 ~prec ((x,I.of_dyadic m1)::env) bs1 then m1 else a) in
+	      let b' = (if l2 ~prec ((x,I.of_dyadic m2)::env) bs2 then m2 else b) in
+	      let j = I.make a' b' in 	    
+	      let env' = (x,j)::env in
+	      let q1 = r1 ~k ~prec env' bs1 in
+	      let q2 = r2 ~k ~prec env' bs2 in
+	      [Cut (x,j,q1,q2)] 
+	  | _ -> error "cutref"
   and 
-    getx x bs = match bs with | (Quant (x,i,_))::tail -> i  | (Cut (x,i,_,_))::tail -> i | _::tail -> getx x tail | [] -> error ("getx")
+    partitionlist len lst = 
+      if len == 0 then [],lst
+      else
+	match lst with 
+	  | h::tl -> let a,b = partitionlist (len-1) tl in (h::a),b
+	  | _ -> error "partitionlist"
+  and
+    binref l1 r1 r2 = fun ~k ~prec env bs -> let bs1,bs2 = partitionlist l1 bs in (r1 ~k ~prec env bs1) @ (r2 ~k ~prec env bs2)
+  and 
+    getx x env = List.assoc x env
   and
     compile_real env e = match e with
 	| S.Dyadic d -> let i = I.of_dyadic d in 
-	      (fun ~prec ~round bs -> i), idref, []
+	      (fun ~prec ~round env bs -> i), idref, []
 	| S.Cut (x, i, p1, p2) ->	    	    	    
-	    let _,_,_,bs1 = compile_sigma ((x,realvar x)::env) p1 in
-	    let _,_,_,bs2 = compile_sigma ((x,realvar x)::env) p2 in
-	      (fun ~prec ~round bs -> getx x bs), idref, [Cut (x,i,bs1,bs2)]
+	    let l1,_,r1,bs1 = compile_sigma ((x,realvar x)::env) p1 in
+	    let l2,_,r2,bs2 = compile_sigma ((x,realvar x)::env) p2 in
+	      (fun ~prec ~round env bs -> match bs with | (Cut (_,i,_,_))::_ -> i | _ -> error ""), 
+	      (cutref l1 r1 l2 r2), 
+	      [Cut (x,i,bs1,bs2)]
 	| S.Binary (op, e1, e2) -> 
 	    let est1,r1,bs1 = compile_real env e1 in
 	    let est2,r2,bs2 = compile_real env e2 in	    
@@ -74,59 +111,69 @@ struct
 	      | S.Minus -> I.sub
 	      | S.Times -> I.mul
 	      | S.Quotient -> I.div) in
-	      (fun ~prec ~round bs -> opf ~prec ~round (est1 ~prec ~round bs) (est2 ~prec ~round bs)),
-		  idref,
+	    let l1 = List.length bs1 in
+	      (fun ~prec ~round env bs -> let bs1,bs2 = partitionlist l1 bs in opf ~prec ~round (est1 ~prec ~round env bs1) (est2 ~prec ~round env bs2)),
+		  binref l1 r1 r2,
 		  bs1@bs2
 	| S.Unary (op, e) ->	    
 	    let est1,r1,bs1 = compile_real env e in	    
 	    let opf = (match op with
 	      | S.Opposite -> I.neg 
 	      | S.Inverse -> I.inv) in
-	      (fun ~prec ~round bs -> opf ~prec ~round (est1 ~prec ~round bs)),
-		idref,
+	      (fun ~prec ~round env bs -> opf ~prec ~round (est1 ~prec ~round env bs)),
+		r1,
 		bs1
 	| S.Power (e, k) ->	    
 	    let est1,r1,bs1 = compile_real env e in	    
-	      (fun ~prec ~round bs -> I.pow ~prec ~round (est1 ~prec ~round bs) k), idref, bs1
+	      (fun ~prec ~round env bs -> I.pow ~prec ~round (est1 ~prec ~round env bs) k),
+	      r1, bs1
 	| _ -> (match compile env e with
 	    | Real (est,r,bs) -> est,r,bs
 	    | _ -> error ("typecheck" ^ S.string_of_expr e))
   and
-    realvar x = Real ((fun ~prec ~round bs -> getx x bs), idref, [])
+    realvar x = Real ((fun ~prec ~round env bs -> getx x env), idref, [])
+  and
+    foldop op sop env lst = match lst with
+      | h::tl -> let l1,u1,r1,bs1 = compile_sigma env h in
+		let l2,u2,r2,bs2 = foldop op sop env tl in
+		let len1= List.length bs1 in
+		(fun  ~prec env bs -> let bs1,bs2 = partitionlist len1 bs in op (l1  ~prec env bs1) (l2  ~prec env bs2)),
+		(fun  ~prec env bs -> let bs1,bs2 = partitionlist len1 bs in op (l1  ~prec env bs1) (l2  ~prec env bs2)),
+		binref len1 r1 r2,
+		bs1@bs2
+      | [] -> (fun  ~prec env bs -> sop), (fun  ~prec env bs -> sop), idref, []
   and
     compile_sigma env e = match e with
-	| S.True -> ((fun bs -> true), (fun bs -> true), idref, [])
-	| S.False -> ((fun bs -> false), (fun bs -> false), idref, [])
+	| S.True -> ((fun ~prec env bs -> true), (fun  ~prec env bs -> true), idref, [])
+	| S.False -> ((fun  ~prec env bs -> false), (fun  ~prec env bs -> false), idref, [])
 	| S.Less (e1, e2) -> 	    
 	    let est1,r1,bs1 = compile_real env e1 in
-	    let est2,r2,bs2 = compile_real env e1 in	
-	    ((fun bs -> 
-	      let i1 = est1 ~prec:1 ~round:D.down bs in
-	      let i2 = est2 ~prec:1 ~round:D.down bs in D.lt (I.upper i1) (I.lower i2)),
-		    (fun bs -> 
-	      let i1 = est1 ~prec:1 ~round:D.up bs in
-	      let i2 = est2 ~prec:1 ~round:D.up bs in D.lt (I.upper i1) (I.lower i2)),
-	      idref, bs1@bs2)
-	| S.And lst -> let lst = (List.map (compile_sigma env) lst) in
-	  let lower = List.fold_left (fun h x -> let l,u,r,bs = x in fun bs -> h bs && l bs) (fun env -> true) lst in
-	  let upper = List.fold_left (fun h x -> let l,u,r,bs = x in fun bs -> h bs && u bs) (fun env -> true) lst in
-	  let bs = List.fold_left (fun h x -> let l,u,r,bs = x in h@bs) [] lst in
-	    (lower, upper, idref, bs)	    
-	| S.Or lst -> let lst = (List.map (compile_sigma env) lst) in
-	  let lower = List.fold_left (fun h x -> let l,u,r,bs = x in fun bs -> h bs || l bs) (fun env -> false) lst in
-	  let upper = List.fold_left (fun h x -> let l,u,r,bs = x in fun bs -> h bs || u bs) (fun env -> false) lst in
-	  let bs = List.fold_left (fun h x -> let l,u,r,bs = x in h@bs) [] lst in
-	    (lower, upper, idref, bs)	    	    
+	    let est2,r2,bs2 = compile_real env e2 in
+	    let l1 = List.length bs1 in
+	    ((fun ~prec env bs -> 
+	      let bs1,bs2 = partitionlist l1 bs in
+	      let i1 = est1 ~prec ~round:D.down env bs1 in
+	      let i2 = est2 ~prec ~round:D.down env bs2 in 
+		  D.lt (I.upper i1) (I.lower i2)) , 
+		    (fun ~prec env bs -> 
+	      let bs1,bs2 = partitionlist l1 bs in
+	      let i1 = est1 ~prec ~round:D.up env bs1 in
+	      let i2 = est2 ~prec ~round:D.up env bs2 in D.lt (I.upper i1) (I.lower i2)),
+	      binref l1 r1 r2, bs1@bs2)
+	| S.And lst -> 
+	    foldop (&&) true env lst
+	| S.Or lst -> 
+	    foldop (||) false env lst
 	| S.Exists (x, i, e) -> 	    
 	    let l,u,r,bs = compile_sigma ((x,realvar x)::env) e in
-	    ((fun bs -> true), (fun bs-> false), idref, [Quant (x,i,bs)])
+	    ((fun  ~prec env bs -> true), (fun  ~prec env bs-> false), idref, [Quant (x,[i,bs])])
 	    (*let est1,r1,bs1 = commonreal real1 in
 	    let s,l,u = compile_sigma ((x,realvar x)::env) e in
 	      ([Quant (x,[(i,s)])], (fun env -> let m = I.of_dyadic (I.midpoint ~prec:1 1 (List.assoc x env)) in l ((x,m)::env)),
 			fun env -> let j = I.flip (List.assoc x env) in u ((x,j)::env))*)
 	| S.Forall (x, i, e) -> (*Forall (x, i, compile_sigma ((x,realvar x)::env) e)	  *)	    
 	    let l,u,r,bs = compile_sigma ((x,realvar x)::env) e in
-	    ((fun bs -> true), (fun bs-> false), idref, [Quant (x,i,bs)])
+	    ((fun  ~prec env bs -> true), (fun  ~prec env bs-> false), idref, [Quant (x,[i,bs])])
 
 (*	    let s,l,u = compile_sigma ((x,realvar x)::env) e in
 	      ([Quant (x,[(i,s)])], (fun env -> let m = I.of_dyadic (I.midpoint ~prec:1 1 (List.assoc x env)) in l ((x,m)::env)),
@@ -138,13 +185,40 @@ struct
  (** Convert a string to expression *)
   let rec string_of_expr e =
     match e with
-	  | Real (_,_,bs) -> "real: " ^ str_of_bs bs 
+	  | Real (e,_,bs) -> let i = e ~prec:32 ~round:D.down [] bs in	"real " ^ I.to_string_number i ^ ":" ^ str_of_bs bs 
 	  | Sigma (_,_,_,bs) -> "sigma: " ^ str_of_bs bs
 	  | Tuple lst -> "(" ^ (String.concat ", " (List.map string_of_expr lst)) ^ ")"
 	  | Uncompiled e -> "["^(S.string_of_expr e)^"]"
    and str_of_bs bs = "(" ^ String.concat ", " (List.map str_of_env bs) ^ ")"
+   and str_of_quant ibslst = "(" ^ String.concat ", " (List.map (fun (i,bs) -> I.to_string i ^ str_of_bs bs) ibslst) ^ ")"
    and str_of_env env = match env with
-      | Quant (x, i, bs) -> "q " ^ (S.string_of_name x) ^ ":" ^ (I.to_string i) ^ ":" ^ str_of_bs bs
+      | Quant (x, ibs) -> "q " ^ (S.string_of_name x) ^ ":" ^ str_of_quant ibs
       | Cut (x, i, bs1, bs2) -> "cut " ^ (S.string_of_name x) ^ ":" ^ (I.to_string i) ^ " left " ^ str_of_bs bs1 ^ " right " ^ str_of_bs bs2
+
+  let target_precision = ref (D.make_int ~prec:10 ~round:D.up 1 (-52))
+
+  let exec trace e =  
+    let rec loop k p e' =
+      if trace then
+	begin
+	  print_endline ("--------------------------------------------------\n" ^
+			   "Iteration: " ^ string_of_int k ^ "\n" ^
+			   string_of_expr e' ^ "\n" ^
+			   "Press Enter to continue " 
+			) ;
+	  ignore (read_line ())	  
+	end ;
+      match e' with
+	| Real (e,r,bs) ->
+	    let i = e ~prec:p ~round:D.down [] bs in	       
+	    let w = (I.width 10 D.up i) in	    
+	      if D.lt w !target_precision then
+		(e', Real (e,r,bs))
+	      else
+		loop (k+1) (make_prec (p+3) (I.make D.zero !target_precision)) (Real (e,r,r ~k ~prec:p [] bs))		
+	|  _ -> (e', e')
+    in            
+      loop 1 32 e
+
   
 end;;
